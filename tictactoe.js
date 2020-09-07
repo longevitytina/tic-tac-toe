d08e14a7cdc5ed81e0c13b7ba0f1498512c03eb4
Tina <longevity.tina@gmail.com>
2020-09-07 16:41:54 -0700

    Fixes checkVertical bug, cleans up

--- tictactoe.js
+++ tictactoe.js
@@ -1,12 +1,4 @@
 //TicTacToe
-// play on terminal, print the gameBoard after every turn
-// change the player after every turn
-//      keep track of who is playing
-// replace the tile selected with x or o
-// game over when three in a row or all tiles are filled
-
-// gamestart function
-//      prompts to select a tile
 
 var readline = require("readline");
 const { question } = require("readline-sync");
@@ -14,9 +6,6 @@ const { question } = require("readline-sync");
 const game = {
   player: "x",
   gameBoard: [
-    // ["x", "1,0", "x"],
-    // ["o", "x", "2,1"],
-    // ["x", "o", "o"],
     ["0,0", "1,0", "2,0"],
     ["0,1", "1,1", "2,1"],
     ["0,2", "1,2", "2,2"],
@@ -40,7 +29,6 @@ const game = {
   init() {
     //starts game, displays board, initializes first move, continues game until winner is found
     // TODO:
-    // - Repeat turns
     // - Handle errors / bad user input
     // - Bonus: make an A.I
     this.question();
@@ -52,7 +40,6 @@ const game = {
     const [x, y] = answer.split(",");
     this.choose(x, y);
   },
-  // use rl.question to replace numbered tile with current player's symbol(x or o)
   choose(x, y) {
     this.gameBoard[y][x] = this.player;
     if (this.checkWin()) {
@@ -81,46 +68,19 @@ const game = {
   },
 
   checkVertical() {
-    let count = [];
     for (let i = 0; i < this.gameBoard.length; i++) {
+      const count = [];
       for (let j = 0; j < this.gameBoard[i].length; j++) {
-        if (this.gameBoard[j][i] === this.player) {
-          count.push(i);
-        }
+        count.push(this.gameBoard[j][i]);
+      }
+      if (count.every((columnIndex) => this.player === columnIndex)) {
+        return true;
       }
     }
-    // console.log(count);
-    // check if all index(colums) are the same
-    if (count.length >= 3) {
-      // This still has a problem :( e.g: [0, 1, 1, 1, 2]
-      return count.every((columnIndex) => count[0] === columnIndex);
-    } else {
-      return false;
-    }
+    return false;
   },
 
   checkDiagonal() {
-    // gameBoard: [
-    //   ["x", "o", "x"], 0,0 1,0 2,0    0,0 1,1 0,2
-    //   ["0,1", "x", "2,1"],
-    //   ["0,2", "1,2", "2,2"],
-    // ],
-    // 0,0  1,1  2,2
-    // 2,0  1,1 0,2
-    // if columns have all different x or y coordinates
-    this.printBoard();
-    let count = [];
-    // You copied this from your checkVertical method, but there's tiny (but important) thing you need to change
-    for (let i = 0; i < this.gameBoard.length; i++) {
-      for (let j = 0; j < this.gameBoard[i].length; j++) {
-        if (this.gameBoard[i][j] === this.player) {
-          // push coordinates, sort and return true if specific sequence is met
-          // 0,0  1,1  2,2, || 2,0  1,1 0,2
-          // 00 02 11 20 22
-          count.push(j, i);
-        }
-      }
-    }
     // Start by assuming that the player is goin to win
     let isWin = true;
     for (let i = 0; i < this.gameBoard.length; i++) {
@@ -135,33 +95,7 @@ const game = {
         isWin = false;
     }
     return isWin;
-
-    // let isWin = true
-    // let i = 0
-    // let j = 2
-    // while (i < this.gameBoard.length) {
-    //   if (this.gameBoard[i++][j--] !== this.player) isWin = false;
-    // }
-    // return isWin;
-
-    // if (count.length >= 3) {
-    //   console.log(count);
-    //   // Think of all the possible permutations of three items
-    //   return (
-    //     count === [0, 0, 1, 1, 2, 2] || count === [2, 0, 1, 1, 0, 2]
-    //     // count[0] != count[1] && count[0] != count[2] && count[1] != count[2]
-    //   );
-    // } else {
-    //   return false;
-    // }
   },
 };
 
-// console.log(game.checkDiagonal());
-// console.log(game.checkWin());
-// console.log(game.checkVertical());
-// console.log(game.checkHorizontal());
 game.init();
-
-//create input function
-//  make it synchr.w/ promises, async await
